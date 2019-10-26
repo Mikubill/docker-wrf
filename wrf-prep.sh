8992fe201da24721b56a20f33ae8ddeeb739f111
Shimaro Kaze <31246794+Mikubill@users.noreply.github.com>
2019-10-26 12:55:51 +0800

    Update wrf-prep.sh

--- wrf-prep.sh
+++ wrf-prep.sh
@@ -6,9 +6,10 @@ which CC
 which CXX
 which F77
 cd /root/
+git clone https://github.com/wrf-model/WRF
+git clone https://github.com/wrf-model/WPS
 mkdir /root/work
 mkdir /root/work/geog
 curl -SL http://www2.mmm.ucar.edu/wrf/src/wps_files/geog_low_res_mandatory.tar.gz | tar -xzC /root/work/geog
 curl -SL http://www2.mmm.ucar.edu/wrf/src/wps_files/geog_high_res_mandatory.tar.gz | tar -xzC /root/work/geog
-git clone https://github.com/wrf-model/WRF
-git clone https://github.com/wrf-model/WPS
+
